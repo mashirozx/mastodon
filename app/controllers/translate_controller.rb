3430cc0a0da8b583f7b66b57229adc26b57bcb11
mashiro <moezhx@outlook.com>
2020-08-19 13:47:40 +0800

    some test

--- app/controllers/translate_controller.rb
+++ app/controllers/translate_controller.rb
@@ -9,4 +9,12 @@ class TranslateController < ApplicationController
     resp = Faraday.post(translation_endpoint, text: params[:data][:text], to: params[:data][:to])
     render json: ActiveSupport::JSON.decode(resp.body)
   end
+
+  # def gets
+  #   return unless user_signed_in?
+  #   translation_endpoint = ENV['TRANSLATION_SERVER_HOST'] || 'http://localhost:30031'
+
+  #   resp = Faraday.post(translation_endpoint, text: 'hello', to: 'ja')
+  #   render json: ActiveSupport::JSON.decode(resp.body)
+  # end
 end
