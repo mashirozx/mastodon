3ec44fea2b31c21794d93f11a145abef03347ba0
mashiro <adadam@qq.com>
2020-10-07 05:13:15 +0800

    use pro

--- app/javascript/mastodon/common.js
+++ app/javascript/mastodon/common.js
@@ -1,8 +1,8 @@
 import Rails from '@rails/ujs';
 
 export function start() {
-  require('@fortawesome/fontawesome-free/css/all.css');
-  require('@fortawesome/fontawesome-free/css/v4-shims.css');
+  require('@moezx/fontawesome-pro/css/all.css');
+  require('@moezx/fontawesome-pro/css/v4-shims.css');
   require.context('../images/', true);
 
   try {
