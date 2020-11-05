206410602cd3bbb48f330443fb22ed0ab5f6f083
mashiro <adadam@qq.com>
2020-11-05 13:32:46 +0800

    fix detect-passive-events in dropdown containers

--- app/javascript/mastodon/features/compose/components/content_type_dropdown.js
+++ app/javascript/mastodon/features/compose/components/content_type_dropdown.js
@@ -5,7 +5,7 @@ import IconButton from '../../../components/icon_button';
 import Overlay from 'react-overlays/lib/Overlay';
 import Motion from '../../ui/util/optional_motion';
 import spring from 'react-motion/lib/spring';
-import detectPassiveEvents from 'detect-passive-events';
+import { supportsPassiveEvents } from 'detect-passive-events';
 import classNames from 'classnames';
 import Icon from 'mastodon/components/icon';
 
@@ -15,7 +15,7 @@ const messages = defineMessages({
   change_content_type: { id: 'content_type.change', defaultMessage: 'Adjust status content type' },
 });
 
-const listenerOptions = detectPassiveEvents.hasSupport ? { passive: true } : false;
+const listenerOptions = supportsPassiveEvents ? { passive: true } : false;
 
 class ContentTypeDropdownMenu extends React.PureComponent {
 
